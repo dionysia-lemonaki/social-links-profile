1198e452e4222b73b3a0aa72e4cc23305edeefbd
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-07-17 14:31:33 +0300

    style: style profile card layout and text

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -7,14 +7,26 @@ interface ProfileCardProps {
 
 const ProfileCard = ({ person }: ProfileCardProps) => {
   return (
-    <div>
-      <img src={person.avatar} alt="" width="88" height="88" />
-      <div>
-        <h1>{person.name}</h1>
-        <p>{person.location}</p>
+    <div className="bg-grey-800 max-w-[24rem] w-full rounded-xl p-6 md:p-10 flex flex-col gap-6 text-center">
+      <img
+        src={person.avatar}
+        alt=""
+        width="88"
+        height="88"
+        className="mx-auto rounded-full"
+      />
+      <div className="flex flex-col gap-1">
+        <h1 className="text-2xl font-semibold leading-normal text-white">
+          {person.name}
+        </h1>
+        <p className="text-sm font-bold leading-normal text-green">
+          {person.location}
+        </p>
       </div>
-      <p>{person.bio}</p>
-      <ul>
+      <p className="text-sm font-normal leading-normal text-white before:content-(--content) after:content-(--content)">
+        {person.bio}
+      </p>
+      <ul className="flex flex-col gap-4">
         {person.links.map((link) => (
           <ProfileLink key={link.url} link={link} />
         ))}
