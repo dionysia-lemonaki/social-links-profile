e556184c30c9b6a0546f55060f62567c2cfff7a2
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-07-17 15:12:43 +0300

    style: style profile links with hover and focus states

--- src/components/ProfileLink.tsx
+++ src/components/ProfileLink.tsx
@@ -7,7 +7,11 @@ interface ProfileLinkProps {
 const ProfileLink = ({ link }: ProfileLinkProps) => {
   return (
     <li>
-      <a href={link.url} target="_blank">
+      <a
+        href={link.url}
+        target="_blank"
+        className="block bg-grey-700 p-3 rounded-lg text-white text-sm font-bold leading-normal hover:bg-green hover:text-grey-700 focus-visible:outline-2 focus-visible:outline-green focus-visible:outline-dotted focus-visible:outline-offset-4"
+      >
         {link.label}
         <span className="sr-only">(Opens in a new tab)</span>
       </a>
